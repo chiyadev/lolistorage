27f65bef2a15dbf9bc545ca751fcedf6e9e0cfd4
phosphene47 <phosphene47@chiya.dev>
2020-08-26 18:37:19 +1000

    Implement file endpoint

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,36 @@
+use rocket::{http::Status, response::Responder, Request, Response};
 use std::{error, fmt};
 
 #[derive(Debug)]
 pub enum LoliError {
-    // Reqwest(reqwest::Error),
-    Unknown,
+    RusotoS3(rusoto_s3::S3Error),
+    // Unknown,
 }
 
 impl error::Error for LoliError {}
 
-// impl From<reqwest::Error> for LoliError {
-//     fn from(err: reqwest::Error) -> LoliError {
-//         LoliError::Reqwest(err)
-//     }
-// }
+impl From<rusoto_s3::S3Error> for LoliError {
+    fn from(err: rusoto_s3::S3Error) -> LoliError {
+        LoliError::RusotoS3(err)
+    }
+}
 
 impl fmt::Display for LoliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            // LoliError::Reqwest(err) => err.fmt(f),
-            LoliError::Unknown => write!(f, "unknown error"),
+            LoliError::RusotoS3(err) => write!(
+                f,
+                "{} ({})",
+                err.message.clone().unwrap_or(String::new()),
+                err.code.clone().unwrap_or(String::new())
+            ),
+            // LoliError::Unknown => write!(f, "unknown error"),
         }
+    }
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for LoliError {
+    fn respond_to(self, request: &'r Request<'_>) -> Result<Response<'o>, Status> {
+        Status::InternalServerError.respond_to(request)
     }
 }
--- src/file.rs
+++ src/file.rs
@@ -1,8 +1,30 @@
-use rocket::get;
-use rocket_contrib::json::Json;
+use crate::{config::CONFIG, storage::STORAGE};
+use log::debug;
+use rocket::{get, response::Stream, tokio::io::AsyncRead};
+use rusoto_s3::{GetObjectRequest, S3};
 use std::path::PathBuf;
 
 #[get("/files/<path..>")]
-pub async fn file(path: PathBuf) -> Json<()> {
-    Json(())
+pub async fn file(path: PathBuf) -> Option<Stream<impl AsyncRead>> {
+    let key = path.to_string_lossy().into_owned();
+
+    match STORAGE
+        .get_object(GetObjectRequest {
+            bucket: CONFIG.s3.bucket.clone(),
+            key: key.clone(),
+            ..Default::default()
+        })
+        .await
+    {
+        Ok(result) => {
+            if let Some(body) = result.body {
+                return Some(Stream::chunked(body.into_async_read(), 16384));
+            }
+        }
+        Err(err) => {
+            debug!("could not read file {}: {}", key, err);
+        }
+    }
+
+    None
 }
