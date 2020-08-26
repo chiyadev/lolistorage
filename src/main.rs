33cc62aff4a98a01251cff3740b83fc5cf7cda74
phosphene47 <phosphene47@chiya.dev>
2020-08-26 18:11:48 +1000

    Fix logger not being initialized

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
+use crate::config::CONFIG;
 use rocket::{get, ignite, launch, routes, Rocket};
 use rocket_contrib::templates::Template;
 use std::path::PathBuf;
 
 mod api;
-mod storage;
 mod config;
 mod dir;
 mod error;
 mod file;
+mod storage;
 
 #[launch]
 fn rocket() -> Rocket {
+    env_logger::init_from_env(env_logger::Env::default().filter_or(
+        "LOLI_LOG",
+        if cfg!(debug_assertions) {
+            "debug"
+        } else {
+            "warn"
+        },
+    ));
+
+    // trigger initial config load
+    let _ = CONFIG;
+
     ignite()
         .mount("/", routes![index, dir::dir, file::file, api::api])
         .attach(Template::fairing())
