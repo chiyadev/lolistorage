f3135ad18e5c3df535ba13778f1284b106460f17
phosphene47 <phosphene47@chiya.dev>
2020-08-26 23:38:59 +1000

    Allow config access in templates

--- src/dir.rs
+++ src/dir.rs
@@ -1,7 +1,18 @@
-use crate::api::api;
+use crate::{
+    api::{api, List},
+    config::{Configuration, CONFIG},
+};
 use rocket::get;
 use rocket_contrib::templates::Template;
+use serde::Serialize;
 use std::path::PathBuf;
+
+#[derive(Serialize)]
+struct ViewContext {
+    title: String,
+    config: &'static Configuration,
+    list: List,
+}
 
 #[get("/view?<begin>")]
 pub async fn dir_index(begin: Option<String>) -> Template {
@@ -10,7 +21,18 @@ pub async fn dir_index(begin: Option<String>) -> Template {
 
 #[get("/view/<path..>?<begin>")]
 pub async fn dir(path: PathBuf, begin: Option<String>) -> Template {
+    let title = path
+        .file_name()
+        .map_or(String::new(), |s| s.to_string_lossy().into_owned());
+
     let list = api(path, begin).await.into_inner();
 
-    Template::render("view", list)
+    Template::render(
+        "view",
+        &ViewContext {
+            title,
+            config: &CONFIG,
+            list,
+        },
+    )
 }
